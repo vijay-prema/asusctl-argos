dd5b649e3599c8ade585ce7e27cd4d48016a6844
vijay-prema <vjprema@gmail.com>
2022-03-16 11:57:05 +1300

    reset leds, update superfxctl

--- asus.+.sh
+++ asus.+.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 GFX=$(supergfxctl -g)
-STATE=$(supergfxctl -p)
+STATE=$(supergfxctl -S)
 PROF=$(asusctl profile -p)
 BATT=$(cat /etc/asusd/asusd.conf | tr -dc '0-9')
 
-echo "| iconName=profile-boost"
+echo "| iconName=profile-performance"
 echo "---"
 echo "GPU:\t\t ${GFX^} ($STATE) | iconName=nvidia"
 echo "-- Integrated | bash='supergfxctl -m Integrated' terminal=false"
@@ -21,4 +21,6 @@ echo "Charge Limit:\t $BATT% | iconName=battery"
 echo "-- 60% | bash='asusctl -c 60' terminal=false"
 echo "-- 80% | bash='asusctl -c 80' terminal=false"
 echo "-- 100% (OFF) | bash='asusctl -c 100' terminal=false"
+echo "---"
+echo "Reset Keyboard LEDs | bash='asusctl led-mode static' terminal=false iconName=keyboard"
 
